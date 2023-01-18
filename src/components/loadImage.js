3ba4123602f74ef2481afe6e3e5fca175a331052
SpyridonKaperonis <spyridonkaperonis@gmail.com>
2023-01-17 21:27:18 -0500

    module changes

--- src/components/loadImage.js
+++ src/components/loadImage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export const LoadImage = (props) => {
-    const source = props.source;
+    const source_ = props.source;
     const alt_ = props.alt_;
     const style = props.style;
 
   return (
-    <>
-        <img style={style} src={source} alt={alt_}/>
-    </>
-  )
-}
+    <nav>
+    <div style={{width: '100%', height: '100%'}}>
+      <img style={style} src={source_} alt={alt_}/>
+    </div>
+    </nav>
+  );
+};
--- src/pages/intro.js
+++ src/pages/intro.js
@@ -8,20 +8,30 @@ import { NameHeadline } from '../components/nameHealine'
 export const Intro = () => {
   return (
     <>
+      <div style={styles.container}>
       <NameHeadline name={'Spyrion Kaperonis'} />
       <LoadImage 
         style={styles.image}
-        source={'./images/skimage.jpg'} 
+        source={'/src/images/image14.png'}
         alt_={'Spyridon Kaperonis'}
       />
+      </div>
     </>
   )
 }
 
 
 const styles = {
+
+  container: {
+    displaye: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   image:{
-    width: '10vh',
-    height: '10vh',
+    width: '40px',
+    height: '40px',
   }
 }
